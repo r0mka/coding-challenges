6c85ed61bdb2528fa71bc1b911dacd3246084360
r0mka <romanliveshere@gmail.com>
2020-10-25 01:02:12 -0700

    selection sort algo

---
+++ sort/selection.js
@@ -0,0 +1,22 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[minIdx] > arr[j]) {
+        minIdx = j;
+      }
+    }
+    [arr[minIdx], arr[i]] = [arr[i], arr[minIdx]];
+  }
+  return arr;
+}
+
+console.log(
+  JSON.stringify(selectionSort([5, 1, 10, 45, 23, 15, 9])) ===
+    '[1,5,9,10,15,23,45]'
+);
+
+console.log(
+  JSON.stringify(selectionSort([10, 9, 8, 23, -19, 5])) ===
+    '[-19,5,8,9,10,23]'
+);

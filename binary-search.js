b1795fea6e5b26a15aa7950196af330d86b030a0
r0mka <romanliveshere@gmail.com>
2020-10-24 23:42:33 -0700

    binary-search

--- binary-search.js
+++ binary-search.js
@@ -8,11 +8,9 @@ function search(arr, value) {
     if (arr[middle] > value) max = middle - 1;
     else min = middle + 1;
   }
-  return 'not present in this array';
+  return -1;
 }
 
 console.log(search([2, 3, 4, 5, 6, 123, 989, 1000], 1000) === 7);
 console.log(search([1, 2, 3, 6, 7, 10, 12], 3) === 2);
-console.log(
-  search([5, 32, 43, 57], 570) === 'not present in this array'
-);
+console.log(search([5, 32, 43, 57], 570) === -1);

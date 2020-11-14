579ae2e0d28eaf6e6d9a5d416a5205499937231d
r0mka <romanliveshere@gmail.com>
2020-11-14 03:49:06 -0800

    Refactor areThereDuplicates using frequencyCounter method

--- colt-steele-course/5-patterns/1-frequency-counter/areThereDuplicates.js
+++ colt-steele-course/5-patterns/1-frequency-counter/areThereDuplicates.js
@@ -2,11 +2,14 @@
  Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
  */
 
-function areThereDuplicates(...args) {
-  const cache = {};
-  for (let i = 0; i < args.length; i++) {
-    if (cache[args[i]]) return true;
-    cache[args[i]] = 1;
+function areThereDuplicates() {
+  let collection = {};
+  for (let val in arguments) {
+    collection[arguments[val]] =
+      (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
   }
   return false;
 }

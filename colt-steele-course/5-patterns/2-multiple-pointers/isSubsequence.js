056fd005564a2b9610b43d3d631bbeaa70521d95
r0mka <romanliveshere@gmail.com>
2020-11-14 05:02:42 -0800

    isSubsequence recursive

--- colt-steele-course/5-patterns/2-multiple-pointers/isSubsequence.js
+++ colt-steele-course/5-patterns/2-multiple-pointers/isSubsequence.js
@@ -24,3 +24,18 @@ console.log(isSubsequence('hello', 'hello world')); // true
 console.log(isSubsequence('sing', 'sting')); // true
 console.log(isSubsequence('abc', 'abracadabra')); // true
 console.log(isSubsequence('abc', 'acb')); // false (order matters)
+
+// isSubsequence Solution - Recursive but not O(1) Space
+function isSubsequence2(str1, str2) {
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str2[0] === str1[0])
+    return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(str1, str2.slice(1));
+}
+
+console.log('=========================');
+console.log(isSubsequence2('hello', 'hello world')); // true
+console.log(isSubsequence2('sing', 'sting')); // true
+console.log(isSubsequence2('abc', 'abracadabra')); // true
+console.log(isSubsequence2('abc', 'acb')); // false (order matters)

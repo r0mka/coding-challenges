13edc07e243e3c4d771493b3b01414c95d132ff6
r0mka <romanliveshere@gmail.com>
2020-11-15 04:17:20 -0800

    minSubarrayLen

--- colt-steele-course/5-patterns/3-sliding-window/minSubarrayLen.js
+++ colt-steele-course/5-patterns/3-sliding-window/minSubarrayLen.js
@@ -8,45 +8,33 @@
 
 // Space Complexity - O(1)
 
-function minSubArrayLen(arr, num) {
-  // keep track of left index and right index
-  let left = 0;
-  let right = 1;
-  // set tempSum to zero
-  let tempSum = arr[0];
-  let tempLength = 1;
-  let minLength = Infinity;
-  // loop over the array
-  while (left < arr.length && right < arr.length) {
-    // add current element to tempSum
-    // increment tempLength
-    // check if tempSum = num
-    tempSum += arr[right];
-    tempLength++;
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
 
-    if (tempSum === num) {
-      if (tempLength < minLength) {
-        minLength = tempLength;
-      }
-      left++;
-      right++;
-      tempSum -= arr[left];
-    } else if (tempSum > num) {
-      // else if tempSum > num
-      if (tempLength < minLength) {
-        minLength = tempLength;
-      }
-      tempSum = tempSum - arr[left] - arr[right];
-      left++;
-      tempLength--;
-    } else {
-      right++;
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then
+    // move the window to right
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
     }
-    // else increment right
+    // if current window adds up to at least the sum given then
+    // we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
   }
-  return minLength === Infinity ? 0 : minLength;
-  // [1, 1, 2, 1, 2, 1, 4, 3] , 7;
-  // [2, 3, 2, 5, 3, 4], 7
+
+  return minLen === Infinity ? 0 : minLen;
 }
 
 console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray

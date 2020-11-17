92d283c29258ea9aa1edf65404c663e7be76c348
r0mka <romanliveshere@gmail.com>
2020-11-17 05:35:55 -0800

    recursion-problem-set

---
+++ colt-steele-course/7-recursion/6-problem-set.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function called power which accepts a base and an exponent. The function
+ * should return the power of the base to the exponent. This function should mimic
+ * the functionality of Math.pow() Do not worry about negative bases and exponents
+ */
+
+function power() {}

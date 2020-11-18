da82e7e7436a19d29b950289d74b0a356cc05d4f
r0mka <romanliveshere@gmail.com>
2020-11-17 21:47:17 -0800

    colt-steele recursion fibonacci

--- colt-steele-course/7-recursion/6-problem-set.js
+++ colt-steele-course/7-recursion/6-problem-set.js
@@ -50,7 +50,23 @@ function recursiveRange(num) {
   return num + recursiveRange(num - 1);
 }
 
-// SAMPLE INPUT/OUTPUT
 console.log('\nRECURSIVE RANGE ===========================');
 console.log(recursiveRange(6)); // 21
 console.log(recursiveRange(10)); // 55
+
+// Write a recursive function called fib which accepts a number
+// and returns the nth number in the Fibonacci sequence.
+// Recall that the Fibonacci is the sequence of whole numbers
+// 1, 1, 2, 3, 5, 8 which starts with 1 and 1, and where every
+// number thereafter is equal to the sum of the previous two numbers.
+
+function fib(num) {
+  if (num <= 2) return 1;
+  return fib(num - 2) + fib(num - 1);
+}
+
+console.log('\nFIBONACHI ===========================');
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465

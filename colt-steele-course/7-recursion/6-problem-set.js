713f75b6ef05637780eea149c3d54e332f6bc988
r0mka <romanliveshere@gmail.com>
2020-11-17 21:41:05 -0800

    colt-steele-course recursion recursiveRange

--- colt-steele-course/7-recursion/6-problem-set.js
+++ colt-steele-course/7-recursion/6-problem-set.js
@@ -9,7 +9,7 @@ function power(base, exp) {
   if (exp === 1) return base;
   return base * power(base, --exp);
 }
-
+console.log('\nPOWER ===========================');
 console.log(power(2, 3)); // 8
 console.log(power(4, 2)); // 16
 console.log(power(5, 1)); // 5
@@ -38,6 +38,19 @@ function productOfArray(arr) {
   return arr[0] * productOfArray(arr.slice(1));
 }
 
-console.log('\nFACTORIAL ===========================');
+console.log('\nPRODUCT OF ARRAY ===========================');
 console.log(productOfArray([1, 2, 3])); // 6
 console.log(productOfArray([1, 2, 3, 10])); // 60
+
+// Write a function called recursiveRange which accepts a number
+// and adds up all the numbers from 0 to the number passed to
+// the function
+function recursiveRange(num) {
+  if (num === 1) return 1;
+  return num + recursiveRange(num - 1);
+}
+
+// SAMPLE INPUT/OUTPUT
+console.log('\nRECURSIVE RANGE ===========================');
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55

de947ac0ace86d624745a5539757fab9f09bc57e
r0mka <romanliveshere@gmail.com>
2020-11-17 21:30:56 -0800

    recursion factorial

--- colt-steele-course/7-recursion/6-problem-set.js
+++ colt-steele-course/7-recursion/6-problem-set.js
@@ -21,8 +21,12 @@ console.log(power(2, 4)); // 16
 // A factorial is the product of an integer and all the integers below it; e.g.,
 // factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24.
 // Factorial zero (0!) is always 1.
-function factorial() {}
-//factorial(1) // 1
-// factorial(2) // 2
-// factorial(4) // 24
-// factorial(7) // 5040
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+console.log('\n\nFACTORIAL ===========================');
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040

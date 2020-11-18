8d9739e8fb5d99e345582898d9bf3ecad80ac6e9
r0mka <romanliveshere@gmail.com>
2020-11-17 21:26:51 -0800

    recursion power function

--- colt-steele-course/7-recursion/6-problem-set.js
+++ colt-steele-course/7-recursion/6-problem-set.js
@@ -4,4 +4,25 @@
  * the functionality of Math.pow() Do not worry about negative bases and exponents
  */
 
-function power() {}
+function power(base, exp) {
+  if (exp === 0) return 1;
+  if (exp === 1) return base;
+  return base * power(base, --exp);
+}
+
+console.log(power(2, 3)); // 8
+console.log(power(4, 2)); // 16
+console.log(power(5, 1)); // 5
+console.log(power(2, 0)); // 1
+console.log(power(2, 2)); // 4
+console.log(power(2, 4)); // 16
+
+// Write a function factorial which accepts a number and returns the factorial of that number.
+// A factorial is the product of an integer and all the integers below it; e.g.,
+// factorial four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24.
+// Factorial zero (0!) is always 1.
+function factorial() {}
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040

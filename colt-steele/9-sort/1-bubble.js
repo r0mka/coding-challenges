35384ef156ade2abf4363e1f8ac50cb3ec10ed48
r0mka <romanliveshere@gmail.com>
2021-01-05 19:41:11 -0800

    Add 1-bubble.js to Colt steeles course sort section

---
+++ colt-steele/9-sort/1-bubble.js
@@ -0,0 +1,52 @@
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort1(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// ES2015 Version
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
+
+// Optimized BubbleSort with noSwaps in case our data is already sorted or almost sorted
+function bubbleOptimized(arr) {
+  var noSwaps;
+  for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleOptimized([8, 1, 2, 3, 4, 5, 6, 7]));

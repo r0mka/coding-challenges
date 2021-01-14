11dbf6a7ebe7282b2668f67d3c4995a865075c48
r0mka <romanliveshere@gmail.com>
2021-01-13 21:58:31 -0800

    Finish mergeSort in colt steele folder

---
+++ colt-steele/9-sort/4-merge.js
@@ -0,0 +1,42 @@
+/*
+    Merge is combination of splitting up sorting and merging.
+    It exploits the fact that arrays of 0 or 1 element are always sorted.
+    Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array.
+ */
+
+// Merges two already sorted arrays
+// Merge function from earlier
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+// Recrusive Merge Sort
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([10, 24, 76, 73]));

1684af9c80d9f989ee6a01f32f89d33b7d330532
r0mka <romanliveshere@gmail.com>
2021-01-16 02:12:55 -0800

    Add push method to singly linked list in colt steele course

--- colt-steele/10-singly-linked-list/1-singly-linked.js
+++ colt-steele/10-singly-linked-list/1-singly-linked.js
@@ -11,6 +11,20 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
+// list.push("HELLO")
+// list.push("GOODBYE")

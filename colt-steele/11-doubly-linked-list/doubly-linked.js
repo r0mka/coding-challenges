3a56d72f1ed5da3f257014acff2e889cac62da57
r0mka <romanliveshere@gmail.com>
2021-01-17 20:21:00 -0800

    Add push method to doubly linked list in colt steele course

--- colt-steele/11-doubly-linked-list/doubly-linked.js
+++ colt-steele/11-doubly-linked-list/doubly-linked.js
@@ -12,4 +12,43 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  print() {
+    let current = this.head;
+    const arr = [];
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+  printReverse() {
+    let current = this.tail;
+    const arr = [];
+    while (current) {
+      arr.push(current.val);
+      current = current.prev;
+    }
+    console.log(arr);
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push(0);
+list.push(1);
+list.push(2);
+list.print();
+list.print();

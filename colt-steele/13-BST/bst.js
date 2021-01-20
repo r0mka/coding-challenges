d6402f3eabbf08d83646d1afa7e7c0e87e640061
r0mka <romanliveshere@gmail.com>
2021-01-19 23:26:08 -0800

    Implement find method in bts in colt steele course

--- colt-steele/13-BST/bst.js
+++ colt-steele/13-BST/bst.js
@@ -10,12 +10,38 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  // iteratively
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
-const tree = new BinarySearchTree();
 
-tree.root = new Node(4);
+const tree = new BinarySearchTree();
 
 tree.root = new Node(10);
 tree.root.right = new Node(15);
 tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
+tree.insert(8);
+console.log(tree);

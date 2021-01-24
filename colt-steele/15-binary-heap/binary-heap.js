0f7f313c6e56a46b17588e7f93b109ed79935a53
r0mka <romanliveshere@gmail.com>
2021-01-24 00:30:08 -0800

    Implement insert method in binary-heap in colt steele course

---
+++ colt-steele/15-binary-heap/binary-heap.js
@@ -0,0 +1,37 @@
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
+
+const heap = new MaxBinaryHeap();
+
+// 41, 39, 33, 18, 27, 12
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+console.log(heap);

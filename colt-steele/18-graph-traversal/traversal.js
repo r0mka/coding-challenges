d694c9f22c7a66eb25658b236a566c291d35ce1a
r0mka <romanliveshere@gmail.com>
2021-01-28 02:29:36 -0800

    Add depthFirst and breadthFirst methods to graph in colt steele course

--- colt-steele/18-graph-traversal/traversal.js
+++ colt-steele/18-graph-traversal/traversal.js
@@ -24,7 +24,64 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
-  depthFirstRecursive() {}
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let g = new Graph();
@@ -43,7 +100,10 @@ g.addEdge('C', 'E');
 g.addEdge('D', 'E');
 g.addEdge('D', 'F');
 g.addEdge('E', 'F');
-g.depthFirstRecursive('A');
+console.log(g);
+console.log(g.depthFirstRecursive('A'));
+console.log(g.depthFirstIterative('A'));
+console.log(g.breadthFirst('A'));
 //          A
 //        /   \
 //       B     C

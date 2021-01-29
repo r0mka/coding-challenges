06c4ce2e87358e0eecf241fe7a584b123f5a3630
r0mka <romanliveshere@gmail.com>
2021-01-29 03:51:49 -0800

    weighted graph in colt steele course

--- colt-steele/19-dijkstra/weightedGraph.js
+++ colt-steele/19-dijkstra/weightedGraph.js
@@ -10,3 +10,24 @@ class WeightedGraph {
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
 }
+
+var graph = new WeightedGraph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
+
+graph.Dijkstra('A', 'E');
+
+// ["A", "C", "D", "F", "E"]

beb68e2408c5f258fa8c9a91b8a7fe49356571c5
r0mka <romanliveshere@gmail.com>
2021-03-17 23:57:08 -0700

    Start sortedSquaredArray problem in algoexpert/js/arrays

---
+++ algoexpert/js/src/arrays/sortedSquaredArray.test.js
@@ -0,0 +1,9 @@
+/**
+ * Write a function that takes in a non-empty array of integers that are sorted
+ * in ascending order and returns a new array of the same length with the
+ * squares of the original integers also sorted in ascending order.
+ */
+
+const sortedSquaredArray = require('./sortedSquaredArray');
+
+it('Test Case #1', () => {});

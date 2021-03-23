cc5dc0517f2c9e5038290d7a33cd20fc06e20d24
r0mka <romanliveshere@gmail.com>
2021-03-23 05:24:23 -0700

    Finish sortedSquaredArray

--- algoexpert/js/src/arrays/sortedSquaredArray.js
+++ algoexpert/js/src/arrays/sortedSquaredArray.js
@@ -4,7 +4,23 @@
  * squares of the original integers also sorted in ascending order.
  */
 function sortedSquaredArray(array) {
-  return array.map((el) => el ** 2);
+  const sortedSquares = new Array(array.length).fill(0);
+  let smallerValueIdx = 0;
+  let largerValueIdx = array.length - 1;
+
+  for (let idx = array.length - 1; idx >= 0; idx--) {
+    const smallerValue = array[smallerValueIdx];
+    const largerValue = array[largerValueIdx];
+
+    if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+      sortedSquares[idx] = smallerValue * smallerValue;
+      smallerValueIdx++;
+    } else {
+      sortedSquares[idx] = largerValue * largerValue;
+      largerValueIdx--;
+    }
+  }
+  return sortedSquares;
 }
 
 module.exports = sortedSquaredArray;
--- algoexpert/js/src/arrays/sortedSquaredArray.test.js
+++ algoexpert/js/src/arrays/sortedSquaredArray.test.js
@@ -6,14 +6,49 @@
 
 const sortedSquaredArray = require('./sortedSquaredArray');
 
-it('Test Case #1', () => {
-  expect(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])).toEqual([
-    1,
-    4,
-    9,
-    25,
-    36,
-    64,
-    81,
-  ]);
+// it('Test Case #1', () => {
+//   expect(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])).toEqual([
+//     1,
+//     4,
+//     9,
+//     25,
+//     36,
+//     64,
+//     81,
+//   ]);
+// });
+
+it('Test Case #2', () => {
+  expect(
+    sortedSquaredArray([-3, -3, -2, -2, 0, 1, 2, 6, 8, 9])
+  ).toEqual([0, 1, 4, 4, 4, 9, 9, 36, 64, 81]);
 });
+
+it('Test Case #3', () => {
+  expect(
+    sortedSquaredArray([-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20])
+  ).toEqual([0, 0, 1, 1, 1, 4, 4, 9, 169, 361, 400, 2500]);
+});
+
+/**
+ * Hint 1
+ * While the integers in the input array are sorted in increasing order, their squares won't necessarily be as well, because of the
+ * possible presence of negative numbers.
+ */
+
+/**
+ * Hint 2
+ * Traverse the array value by value, square each value, and insert the squares into an output array. Then,
+ * sort the output array before returning it. Is this the optimal solution?
+ */
+
+/**
+ * Hint 3
+ * To reduce the time complexity of the algorithms mentioned in Hint #2, you need to avoid sorting the output array.
+ * To do this, as you square the values of the input array, try to directly insert
+ * them into their correct position in the output array.
+ */
+
+/**
+ * O(n) time | O(n) space
+ */

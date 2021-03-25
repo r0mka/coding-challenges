fb4cec50949cb8c2594cc06932415459d1126dc8
r0mka <romanliveshere@gmail.com>
2021-03-25 03:38:45 -0700

    Finish smallestDifference in algoexpert/c++/arrays

--- algoexpert/c++/arrays/smallestDifference.cpp
+++ algoexpert/c++/arrays/smallestDifference.cpp
@@ -9,21 +9,69 @@
    You can assume that there will only be one pair of numbers with the smallest difference.
 
 */
+#include <algorithm>
 #include <iostream>
-void smallestDifference(int arrayOne[], int arrayTwo []) {
-    
-}
+#include <vector>
+// #include <climits>
+
+std::vector<int> smallestDifference(std::vector<int> arrayOne, std::vector<int> arrayTwo);
+
 
 int main() {
+    std::cout << std::endl << std::endl;
     std::cout << "SMALLEST DIFFERENCE" << std::endl;
+
+    // int arr1 [6] =  {-1, 5, 10, 20, 28, 3}; 
+    // int arr2 [5] = {26, 134, 135, 15, 17};
+    
+    // int arr1Size = sizeof(arr1) / sizeof(arr1[0]);
+    // int arr2Size = sizeof(arr2) / sizeof(arr2[0]);
+    
+    // std::vector<int> arrayOne(arr1, arr1 + arr1Size);
+    // std::vector<int> arrayTwo(arr2, arr2 + arr2Size);
+    std::vector<int> arrayOne {-1, 5, 10, 20, 28, 3}; 
+    std::vector<int> arrayTwo {26, 134, 135, 15, 17};
+
+    std::vector<int> smallestPair = smallestDifference(arrayOne, arrayTwo);
+    
+    std::cout << smallestPair[0] << ", " << smallestPair[1] << std::endl;
 }
 
+std::vector<int> smallestDifference(std::vector<int> arrayOne, std::vector<int> arrayTwo) {
+    std::sort(arrayOne.begin(), arrayOne.end());
+    std::sort(arrayTwo.begin(), arrayTwo.end());
+    int idxOne = 0; 
+    int idxTwo = 0; 
+    int smallest = INT_MAX; 
+    int current = INT_MAX;
+    std::vector<int> smallestPair;
+
+    while(idxOne < arrayOne.size() && idxTwo < arrayTwo.size()) {
+        int firstNum = arrayOne[idxOne]; 
+        int secondNum = arrayTwo[idxTwo];
+        if (firstNum < secondNum) {
+            current = secondNum - firstNum; 
+            idxOne++;
+        } else if (secondNum < firstNum) {
+            current = firstNum - secondNum; 
+            idxTwo++;
+        } else {
+           return std::vector<int> {firstNum, secondNum};
+        }
+
+        
+        if (smallest > current) {
+            smallest = current; 
+            smallestPair = {firstNum, secondNum};
+        }
+    }
 
 
 
+    return smallestPair;
+}
 
-
-
+// smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]) -> [28, 26]
 
 
 /*
@@ -38,3 +86,63 @@ int main() {
  the pointer of the smaller of the two numbers to find a potentially better pair. Continue until you get a pair with a difference
  of zero or until one of the pointers gets out of range of its array.
 */
+
+
+
+
+// int* smallestDifference(int arrayOne[], int arrayOneLength,  int arrayTwo [], int arrayTwoLength);
+// void printArray(int array[], int arraySize);
+
+// int main() {
+//     std::cout << std::endl << std::endl;
+//     std::cout << "SMALLEST DIFFERENCE" << std::endl;
+//     int arrayOne[6] = {-1, 5, 10, 20, 28, 3}; 
+//     int arrayOneLength = sizeof(arrayOne)/sizeof(int); 
+//     int arrayTwo[5] = {26, 134, 135, 15, 17};
+//     int arrayTwoLength = sizeof(arrayTwo)/sizeof(int);
+
+//     int* result;
+//     result = smallestDifference(arrayOne, arrayOneLength, arrayTwo, arrayTwoLength);
+
+//     printArray(result, 2);
+//     std::cout << std::endl << std::endl;
+// }
+
+// void printArray(int array[], int arraySize) {
+//     for (int i = 0; i < arraySize; i++) {
+//         std::cout << array[i] << " ";
+//     }
+// }
+
+// int* smallestDifference(int arrayOne[], int arrayOneLength,  int arrayTwo [], int arrayTwoLength) {
+//     std::sort(arrayOne, arrayOne + arrayOneLength);
+//     std::sort(arrayTwo, arrayTwo + arrayTwoLength);
+    
+//     int idxOne = 0; 
+//     int idxTwo = 0; 
+//     int smallestDifference = INT_MAX;
+//     static int pair [2]; 
+    
+    
+
+//     while(idxOne < arrayOneLength && idxTwo < arrayTwoLength) {
+//         if (arrayOne[idxOne] < arrayTwo[idxTwo]) {
+//             int currentDif = arrayTwo[idxTwo] - arrayOne[idxOne]; 
+//             if (currentDif < smallestDifference) {
+//                 smallestDifference = currentDif; 
+//                 pair[0] = arrayOne[idxOne]; 
+//                 pair[1] = arrayTwo[idxTwo];
+//             } 
+//             idxOne++; 
+//         } else {
+//             int currentDif = arrayOne[idxOne] - arrayTwo[idxTwo]; 
+//             if (currentDif < smallestDifference) {
+//                 smallestDifference = currentDif; 
+//                 pair[0] = arrayOne[idxOne]; 
+//                 pair[1] = arrayTwo[idxTwo];
+//             }
+//             idxTwo++; 
+//         }
+//     }
+//     return pair; 
+// }
---
+++ algoexpert/c++/readme.md
@@ -0,0 +1,6 @@
+To compile on mac:
+
+```bsh
+clang++ -std=c++11 smallestDifference.cpp -o result && open result
+
+```
